3ad6bc6f95c28e21859f19a8e6fb1da7c992b0e0
Alejandro <alejandro@fakemail.com>
2023-09-10 10:06:26 +0200

    Added the properties extractor of a video

--- load-results.py
+++ load-results.py
@@ -1,7 +1,50 @@
+import cv2
+
+def get_properties(video_path):
+    properties_names = [
+        'FPS',
+        'FRAME_COUNT',
+        'FRAME_HEIGHT',
+        'FRAME_WIDTH'
+    ]
+
+    vid_cap = cv2.VideoCapture(video_path)
+
+    return {
+        property: vid_cap.get(getattr(cv2, f'CAP_PROP_{property}'))
+        for property in properties_names
+    }
+
+properties = get_properties('videos/combat.mp4')
+print(properties)
+
+exit()
+
+for property in properties:
+    property_name = f'CAP_PROP_{property}'
+    property_value = vid_cap.get(getattr(cv2, property_name))
+
+    print(f'{property_name} = {property_value}')
+
+exit()
+import ffmpeg
+
+media_file = './videos/combat.mp4'
+
+print(dir(ffmpeg))
+adios = ffmpeg.probe(media_file)
+hola = adios["streams"]
+
+print(hola)
+
+exit()
 import joblib
 import numpy as np
 
 frames = joblib.load('combat-capture.sav')
+
+print(len(frames))
+exit()
 
 def get_difference(fighter1, fighter2):
     return np.mean(np.abs(fighter1 - fighter2))
